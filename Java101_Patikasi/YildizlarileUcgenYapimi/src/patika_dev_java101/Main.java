52b65e7ee6be1e7f645aefee30d903b1076cba9f
Mehmet-D-Q1 <83539143+Mehmet-D-Q1@users.noreply.github.com>
2021-09-01 20:15:06 +0300

    Add files via upload

---
+++ Java101_Patikasi/YildizlarileUcgenYapimi/src/patika_dev_java101/Main.java
@@ -0,0 +1,29 @@
+package patika_dev_java101;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Lütfen satır sayısını giriniz: ");
+
+        int row = input.nextInt();
+        for (int i = 1; i <= row; i++) {
+            
+            // ilgili i satırına ait boşlukları basma:
+            for (int j = 1; j <=(row-i); j++) {
+                System.out.print(" ");
+            }
+
+            // ilgili i satırına ait yıldızları basma:
+            for (int k = 1; k <= 2*i-1 ; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
